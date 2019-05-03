9acebc888425696a7a7c267655cc513af00b7519
Emerson Brancher <Brancher81@gmail.com>
2019-05-03 20:05:18 -0200

    App final
    Aula 85 até 96

--- app2/index.js
+++ app2/index.js
@@ -1,27 +1,34 @@
 //imports
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity, Alert } from 'react-native';
 //formatações
 const Estilos = {
-	estiloTexto: {
-		fontSize: 30,
-		backgroundColor: '#08509B',
+	principal: {
+		//fontSize: 30,
+		//backgroundColor: '#538530',
+		flex: 1,
+		justifyContent: 'center',
+		alignItems:'center'
 		//height: 80,
 		//width:300
-		//paddingTop: 40
+		//paddingTop: 40,
 		//paddingLeft: 50
 		//paddingRight: 30
 		//padding: 30
 		//paddingBottom: 30
-		//paddingHorizontal: 30
+		//paddingHorizontal: 20
 		//paddingVertical: 30
-		margin: 30
+		//margin: 30,
 		//marginLeft: 30
 		//marginTop: 30
 		//marginBottom: 30
 		//marginRight: 30
 		//marginVertical: 30
 		//marginHorizontal: 30
+		//color: '#fff',
+		//fontStyle: 'italic'
+		//fontWeight: '900' // valores entre 100 e 900 ou bold
+		//textAlign: 'left'
 
 
 		//Shadow só funciona para IOS
@@ -29,13 +36,59 @@ const Estilos = {
 		shadowOffset: { width: 10, height: 15 },
 		shadowOpacity: 1,
 		shadowRadius: 5*/
+	},
+	botao: {
+		backgroundColor: '#538530',
+		paddingVertical: 10,
+		paddingHorizontal:40,
+		marginTop: 20
+	},
+	textoBotao: {
+		color: 'white',
+		fontSize: 16,
+		fontWeight: 'bold'
 	}
+
+	/*imagem: {
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		paddingBottom: 10
+	}*/
 };
+
+const gerarNovaFrase = () => {
+	var numeroAleatorio = Math.random();
+	numeroAleatorio = Math.floor(numeroAleatorio * 5);
+
+//frases
+	var frases = Array();
+	frases[0] = 'Eu não creio na sua ressurreição, mas não ocultarei a emoção que sinto diante de Cristo e dos seus ensinamentos. Perante Ele e a sua história não experimento senão respeito e reverência.';
+	frases[1] = 'Não acredito que existam qualidades, valores, modos de vida especificamente femininos: seria admitir a existência de uma natureza feminina, quer dizer, aderir a um mito inventado pelos homens para prender as mulheres na sua condição de oprimidas. Não se trata para a mulher de se afirmar como mulher, mas de tornarem-se seres humanos na sua integridade.';
+	frases[2] = 'Creio que aqueles que mais entendem de felicidade são as borboletas e as bolhas de sabão...';
+	frases[3] = 'O ignorante afirma, o sábio duvida, o sensato reflete.';
+	frases[4] = 'Uma vida não questionada não merece ser vivida.';
+
+	var fraseEscolhida = frases [ numeroAleatorio ];
+
+	Alert.alert(null, fraseEscolhida);
+}
 
 //componemte
 const App = () => {
+
+	const { principal, botao, textoBotao } = Estilos;
+
 	return (
-		<Text style={Estilos.estiloTexto}>Frases do dia</Text>
+		<View style={principal}> 
+
+		<Image source={require('./imgs/logo.png')}/>
+		<TouchableOpacity onPress={gerarNovaFrase} style={botao}>
+			<Text style={textoBotao}>
+				Nova Frase
+			</Text>
+		</TouchableOpacity>
+
+		</View>
 	);
 };
 //renderização
